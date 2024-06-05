eda0eb72f4ba71803f32261293be042951e8a356
Dina Cohen <dinahc@balink.net>
2024-06-05 17:56:24 +0300

    task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.entity';
+import { CreateTaskDto } from './create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks : Task[] = [];
+
+    getAllTasks():Task[]{
+        return this.tasks;
+    }
+
+    getTaskById(id: number):Task{
+        return this.tasks.find(task => task.id === id);
+    }
+
+    createTask(createTaskdto :CreateTaskDto) : Task{
+        const {title,description} = createTaskdto;
+        const task : Task = {
+            id : this.tasks.length + 1,
+            title,
+            description,
+            isDone: false
+        };
+        this.tasks.push(task);
+        return task;
+    }
+    
+    updateTask(id: number, updateTaskDto: CreateTaskDto): Task {
+        const task = this.getTaskById(id);
+        task.title = updateTaskDto.title;
+        task.description = updateTaskDto.description;
+        return task;
+      }
+
+    deleteTask(id: number): void{
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+}
