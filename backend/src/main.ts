98497a98e1a7d3407d32a27bae788b839b128235
Dinahkn <73961120+Dinahkn@users.noreply.github.com>
2024-06-16 11:44:47 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { JWTAuthGuard } from './auth/guard/jwt.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new JWTAuthGuard());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
